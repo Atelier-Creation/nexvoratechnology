eaf3dee07dfc3df836f43f57202355e676df5b87
prasanthoyasco <prasanthoyasco@gmai.com>
2026-05-19 15:16:32 +0530

    product and carreer page added

--- replace.js
+++ replace.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const indexContent = fs.readFileSync('index.html', 'utf-8');
 const headerMatch = indexContent.match(/<section class=\"header-section\">[\s\S]*?<\/section>/);
@@ -12,9 +13,33 @@ if (!headerMatch || !footerMatch) {
 const headerContent = headerMatch[0];
 const footerContent = footerMatch[0];
 
-let integrationContent = fs.readFileSync('about-us.html', 'utf-8');
+let integrationContent = fs.readFileSync('./careers/frontend-developer.html', 'utf-8');
 integrationContent = integrationContent.replace(/<section class=\"header-section\">[\s\S]*?<\/section>/, headerContent);
 integrationContent = integrationContent.replace(/<section class=\"footer-section\">[\s\S]*?<\/section>/, footerContent);
 
-fs.writeFileSync('about-us.html', integrationContent, 'utf-8');
-console.log('Successfully replaced header and footer in integration.html');
+fs.writeFileSync('./careers/frontend-developer.html', integrationContent, 'utf-8');
+console.log('Successfully replaced header and footer in frontend-developer.html');
+
+// Replace CSS link in all HTML files
+// const oldLink = 'https://assets-global.website-files.com/65f295c5e095b2886b26ceef/css/saasnap.webflow.25e64d69a.css';
+// const newLink = '../assets/assets-global.website-files.com/65f295c5e095b2886b26ceef/css/saasnap.webflow.25e64d69a.css';
+
+// function replaceLinkInFiles(dir) {
+//     const files = fs.readdirSync(dir);
+//     for (const file of files) {
+//         const filePath = path.join(dir, file);
+//         const stat = fs.statSync(filePath);
+//         if (stat.isDirectory()) {
+//             replaceLinkInFiles(filePath);
+//         } else if (filePath.endsWith('.html')) {
+//             let content = fs.readFileSync(filePath, 'utf-8');
+//             if (content.includes(oldLink)) {
+//                 content = content.split(oldLink).join(newLink);
+//                 fs.writeFileSync(filePath, content, 'utf-8');
+//                 console.log(`Replaced link in ${filePath}`);
+//             }
+//         }
+//     }
+// }
+
+// replaceLinkInFiles(__dirname);
